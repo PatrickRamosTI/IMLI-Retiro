cc8e766ea2ca0f0108dd8a3b5e08ee11c266dc7d
Patrick Ramos <patrickramosTi@gmail.com>
2025-04-14 10:54:47 -0300

    Criando repositorio

---
+++ MIsterio De Louvor backend/IMLIRetiro/src/main/java/com/imli/Retiro/ImilRetiroApplication.java
@@ -0,0 +1,13 @@
+package com.imli.Retiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImilRetiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImilRetiroApplication.class, args);
+	}
+
+}
